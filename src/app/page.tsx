aaa73a1e651376d04f4a95b39a56b6ec28b5fc14
altafkhankpk <145385734+altafkhankpk@users.noreply.github.com>
2025-05-16 16:01:29 +0500

    conflict solve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Overview from "./overview/page";
 
 export default function Home() {
   return (
-    <div></div>
+  <div>
+    <Overview></Overview>
+  </div>
+  
   );
 }
